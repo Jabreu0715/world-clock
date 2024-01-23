9a7375e8d575f7a0fe2ce7d55b28eb8e0ad46747
Jabreu0715 <127450942+Jabreu0715@users.noreply.github.com>
2024-01-23 11:03:01 -0500

    injected moments to html and programmed JS into new york city.

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,10 @@
+//NewYork
+let newYorkElement = document.querySelector("#new-york");
+let newYorkDateElement = newYorkElement.querySelector(".date");
+let newYorkTimeElement = newYorkElement.querySelector(".time");
+let newYorkTime = moment();
 
+newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+newYorkTimeElement.innerHTML = newYorkTime.format(
+  "h:mm:ss [<small>]A[</small>]"
+);
