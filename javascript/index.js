d851e21aa0c7f311780c20f23044db65012fac5d
Jabreu0715 <127450942+Jabreu0715@users.noreply.github.com>
2024-02-02 10:17:40 -0500

    added Moments.js to Jarabacoa and added Set Interval for continuous refresh.

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,25 @@
-//NewYork
-let newYorkElement = document.querySelector("#new-york");
-let newYorkDateElement = newYorkElement.querySelector(".date");
-let newYorkTimeElement = newYorkElement.querySelector(".time");
-let newYorkTime = moment();
+function updateTime() {
+  //NewYork
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkTime = moment().tz("America/New_York");
 
-newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
-newYorkTimeElement.innerHTML = newYorkTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
+  newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do, YYYY");
+  newYorkTimeElement.innerHTML = newYorkTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  //Jarabacoa
+  let jarabacoaElement = document.querySelector("#jarabacoa");
+  let jarabacoaDateElement = jarabacoaElement.querySelector(".date");
+  let jarabacoaTimeElement = jarabacoaElement.querySelector(".time");
+  let jarabacoaTime = moment().tz("America/Santo_Domingo");
+
+  jarabacoaDateElement.innerHTML = jarabacoaTime.format("MMMM Do, YYYY");
+  jarabacoaTimeElement.innerHTML = jarabacoaTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
